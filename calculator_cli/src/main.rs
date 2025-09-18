cf9bff15ae673c904111bd86cd16153a0459b4ec
Ryan <nebulisse@foxmail.com>
2025-09-18 17:34:27 +0800

    初始化项目

--- calculator_cli/src/main.rs
+++ calculator_cli/src/main.rs
@@ -1,15 +1,48 @@
 fn main() {
-    let args = std::env::args();
+    let args: Vec<String> = std::env::args().into_iter().collect();
 
-    parse_args(args);
+    // 参数数量检查
+    if args.len() != 4 {
+        print_usage();
+        return;
+    }
+
+    if args[1] == "--help" {
+        print_usage();
+        return;
+    }
+
+    let calc = parse_args(args.into_iter());
+
+    calc.calculate_print();
 }
 
-fn parse_args(args: std::env::Args) -> Calculator {
-    let first_num = args.skip(1).next().take();
+fn print_usage() {
+    println!("Usage: calc <num1> <op> <num2>");
+    println!("  <op> can be: +, -, *, /");
+}
 
-    println!("first_num: {:?}", first_num);
+fn parse_args(mut args: impl Iterator<Item = String>) -> Calculator {
+    args.next();// 跳过第一个参数，因为它是程序名
+    let first_num: i64 = args.next().expect("first num is required").parse().expect("first num is not a number");
+    let arg_op = args.next().expect("op is required");
+    let op = match arg_op.as_str() {
+        "+" => Operation::Add,
+        "-" => Operation::Subtract,
+        "*" => Operation::Multiply,
+        "/" => Operation::Divide,
+        _ => {
+            eprintln!("op is not valid");
+            print_usage();
+            std::process::exit(1);
+        },
+    };
+    let second_num: i64 = args.next().expect("second num is required").parse().expect("second num is not a number");
 
-    Calculator { first_num: 0, second_num: 0, op: Operation::Add }
+    // println!("first_num: {}", first_num);
+    // println!("op: {:?}", op);
+    // println!("second_num: {}", second_num);
+    Calculator { first_num, second_num, op }
 }
 
 struct Calculator {
@@ -18,6 +51,35 @@ struct Calculator {
     op: Operation,
 }
 
+impl Calculator {
+    fn calculate(&self) -> i64 {
+        match self.op {
+            Operation::Add => self.first_num + self.second_num,
+            Operation::Subtract => self.first_num - self.second_num,
+            Operation::Multiply => self.first_num * self.second_num,
+            Operation::Divide => {
+                if self.second_num == 0 {
+                    eprintln!("divide by zero");
+                    std::process::exit(1);
+                }
+                self.first_num / self.second_num
+            },
+        }
+    }
+
+    fn calculate_print(&self) {
+        let result = self.calculate();
+        let sym = match self.op {
+            Operation::Add => "+",
+            Operation::Subtract => "-",
+            Operation::Multiply => "*",
+            Operation::Divide => "/",
+        };
+        println!("{} {} {} = {}", self.first_num, sym, self.second_num, result);
+    }
+}
+
+#[derive(Debug)]
 enum Operation {
     Add,
     Subtract,
