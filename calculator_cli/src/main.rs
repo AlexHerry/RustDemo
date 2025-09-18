5959d9a68b68143267bf82523a7b756c89686b2b
Ryan <nebulisse@foxmail.com>
2025-09-18 16:22:01 +0800

    初始化项目

--- calculator_cli/src/main.rs
+++ calculator_cli/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    let args = std::env::args();
+
+    parse_args(args);
+}
+
+fn parse_args(args: std::env::Args) -> Calculator {
+    let first_num = args.skip(1).next().take();
+
+    println!("first_num: {:?}", first_num);
+
+    Calculator { first_num: 0, second_num: 0, op: Operation::Add }
+}
+
+struct Calculator {
+    first_num: i64,
+    second_num: i64,
+    op: Operation,
+}
+
+enum Operation {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
 }
